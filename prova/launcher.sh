9f080855fe8210428e2e74e7121d86c6a6ebbc18
3lsesar <cesarmejiarota@gmail.com>
2024-06-09 14:58:23 +0200

    con resultados (el mejor es el model 5)

---
+++ prova/launcher.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --qos=training
+#SBATCH -D .
+#SBATCH --output=test_job_%j.out
+#SBATCH --error=test_job_%j.err
+#SBATCH --cpus-per-task=80			
+#SBATCH --gres gpu:4
+#SBATCH --time=24:00:00
+module purge; module load tensorflow
+python prova.py
+
